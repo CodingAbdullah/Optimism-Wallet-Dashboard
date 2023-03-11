c73dc5545e6c528232699f7f1da788147d50d6c7
Abdul <abdullahmd326@gmail.com>
2023-03-10 22:09:13 -0500

    Footer refined, About page added

--- frontend/src/Components/Footer/footer.tsx
+++ frontend/src/Components/Footer/footer.tsx
@@ -1,25 +1,21 @@
 // Adding footer to project
 import './footer.css';
+import React from 'react';
 
-const Footer = () => {
+const Footer: React.FC = () => {
     const statement = "Copyright "  + new Date().getFullYear() + ". Powered By ";
 
     return (
-        <div className='footer'>
-            <div className="footer footer-container container">
-                <footer className="footer col-md-9 ml-sm-auto col-lg-10 px-md-4">
-                    <div className="container">
-                        <p style={{ marginLeft: '2.5rem', marginTop: '-0.08rem' }} className="copyright-paragraph">
-                            <a style={{ color:'black' }} href="https://docs.alchemy.com/reference/" target="_blank" rel="noreferrer"><img style={{ marginRight: '0.25rem' }} src={ require("../../assets/images/alchemy.png") } width="25" height="25" alt="logo" />Alchemy</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span>
-                            <a style={{ color:'black' }} href="https://www.coingecko.com" target="_blank" rel="noreferrer"><img style={{ marginRight: '0.25rem' }} src={ require("../../assets/images/coingecko.png")} width="25" height="25" alt="logo" />Coin Gecko</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span>
-                            <a style={{ color:'black' }} href="https://optimistic.etherscan.io/" target="_blank" rel="noreferrer"><img style={{ marginRight: '0.25rem' }} src={ require("../../assets/images/optimism.png") } width="25" height="25" alt="logo" />Optimism Etherscan</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span> 
-                            <a style={{ color:'black' }} href="https://moralis.io/" target="_blank" rel="noreferrer"><img style={{ marginRight: '0.25rem' }} src={ require("../../assets/images/moralis.png") } width="25" height="25" alt="logo" />Moralis</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}></span>
-                        </p>
-                        <p style={{ marginLeft: '3.5rem', marginTop: '-0.75rem' }} className="copyright-paragraph">{ statement } <a style={{ color: 'black' }} href="https://reactjs.org/">React</a><img className="footer-logo" src={require("../../assets/images/logo.svg").default} alt="logo" /></p>
-                    </div>
-                </footer>
+        <footer className="pt-5 footer">
+            <div className="container">
+                <p className="copyright-paragraph">
+                    <a style={{ color:'black' }} href="https://www.coingecko.com" target="_blank" rel="noreferrer"><img style={{ marginRight: '0.15rem' }} src={require("../../assets/images/coingecko.png")} width="25" height="25" alt="logo" />Coin Gecko</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}><b>|</b></span>
+                    <a style={{ color:'black' }} href="https://moralis.io/" target="_blank" rel="noreferrer"><img style={{ marginRight: '0.15rem' }} src={require("../../assets/images/moralis.png")} width="25" height="25" alt="logo" />Moralis</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}></span><b>|</b>
+                    <a style={{ color:'black' }} href="https://optimistic.etherscan.io/" target="_blank" rel="noreferrer"><img style={{ marginRight: '0.25rem' }} src={ require("../../assets/images/optimism.png") } width="25" height="25" alt="logo" />Optimism Etherscan</a><span style={{ marginLeft: '0.25rem', marginRight: '0.25rem' }}></span>
+                </p>
+                <p style={{ marginLeft: '0.5rem' }} className="copyright-paragraph">{ statement } <a style={{ color: 'black' }} href="https://reactjs.org/">React</a><img className="footer-logo" src={require("../../assets/images/logo.svg").default} alt="logo" /></p>
             </div>
-        </div>
+        </footer>
     )
 }
 
