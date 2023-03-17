aa1045488250a0ea4deff131a1a76e5eb21f92f5
Abdul <abdullahmd326@gmail.com>
2023-03-16 23:18:00 -0400

    Adding frontend changes for AWS deployment

--- backend/routes/OpERC721HoldingsRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const OpERC721HoldingsController = require("../controllers/OpERC721HoldingsController");
-
-// Adding routes here
-router.post("/op-erc721-holdings", OpERC721HoldingsController.getERC721Holdings);
-router.post("/op-erc721-transfers", OpERC721HoldingsController.getERC721Transfers);
-
-module.exports = router;
