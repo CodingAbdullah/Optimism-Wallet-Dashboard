8c37a0c3e155261ada4f6912b0ebcae6507c4045
CodingAbdullah <abdullahmd326@gmail.com>
2025-02-09 08:12:02 -0500

    Types for Wallet stats, balance, and historical information added

--- nextapp/src/app/utils/constants/Links.ts
+++ nextapp/src/app/utils/constants/Links.ts
@@ -2,9 +2,10 @@ import LinkType from "../types/LinkType";
 
 // Constants for working with Footer Links
 export const Links: LinkType[] = [
-    { name: 'Optimistic Etherscan', href: 'https://docs.optimism.etherscan.io/' },
-    { name: 'CoinGecko', href: 'https://www.coingecko.com/' },
     { name: 'Alchemy', href: 'https://docs.alchemy.com/' },
+    { name: 'CoinGecko', href: 'https://www.coingecko.com/' },
+    { name: 'Moralis', href: 'https://developers.moralis.com/' },
+    { name: 'Optimistic Etherscan', href: 'https://docs.optimism.etherscan.io/' },
     { name: 'Twitter', href: 'https://twitter.com/KA95doteth' },
     { name: 'GitHub', href: 'https://github.com/CodingAbdullah' }
 ];
