8d327ed47d79453874bfbdf61575056f4ffd2741
CodingAbdullah <abdullahmd326@gmail.com>
2025-02-09 08:19:45 -0500

    AWS/MERN stack directories removed

--- backend/routes/OpERC20HoldingsRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const OpERC20HoldingsController = require("../controllers/OpERC20HoldingsController");
-
-// Adding routes here
-router.post("/op-erc20-holdings", OpERC20HoldingsController.getERC20Holdings);
-router.post("/op-erc20-transfers", OpERC20HoldingsController.getERC20Transfers);
-
-module.exports = router;
--- backend/routes/OpGasRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const OpGasPriceController = require("../controllers/OpGasPriceController");
-
-// Add route to fetch gas data
-router.get("/op-gas-tracker", OpGasPriceController.getGasPrice);
-
-module.exports = router;
